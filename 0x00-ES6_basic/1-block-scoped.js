5756c486c70703044c226a7e015f02546803f10b
Davis Tumuhaise <tumuhaise@gmail.com>
2023-06-28 13:39:50 +0300

    fix task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,7 @@
+/* eslint-disable semi */
 export default function taskBlock (trueOrFalse) {
-  const task = false
-  const task2 = true
+  const task = false;
+  const task2 = true;
 
-  return [task, task2]
+  return [task, task2];
 }
