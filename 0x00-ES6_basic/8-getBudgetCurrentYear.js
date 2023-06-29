41518b4b3968605f4ab0b33a1c994f20e88be405
Davis Tumuhaise <tumuhaise@gmail.com>
2023-06-29 12:26:29 +0300

    fix

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -8,8 +8,8 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
 
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita
-};
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 
   return budget;
 }
