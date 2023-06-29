6b5775c5ae6ee9715b2e33bc1ce864b2ebbf6ce7
Davis Tumuhaise <tumuhaise@gmail.com>
2023-06-29 13:33:09 +0300

    fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,16 +1,3 @@
 export default function createReportObject(employeesList) {
-  const employeesObject = {
-    allEmployees: {
-      engineering: [],
-      sales: [],
-      finance: [],
-    },
-    getNumberOfEmployees(department) {
-      return employeesObject.allEmployees[department].length;
-    },
-  };
-  Object.entries(employeesList).forEach((entry) => {
-    employeesObject.allEmployees[entry[1]].push(entry[0]);
-  });
-  return employeesObject;
+  return { allEmployees: { ...employeesList } };
 }
