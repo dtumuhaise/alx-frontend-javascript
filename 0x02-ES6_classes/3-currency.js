03cccad403dbbb9b7db26cfd2b97b6676d50a6f4
Davis Tumuhaise <tumuhaise@gmail.com>
2023-07-07 13:46:28 +0300

    task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code != 'string' || typeof name != 'string') {
+      throw new TypeError('Invalid Type');
+    }
+
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
