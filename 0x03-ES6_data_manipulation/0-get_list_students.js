223c79518834206592630aa1796e6b2846db8950
Davis Tumuhaise <tumuhaise@gmail.com>
2023-07-24 19:41:40 +0300

    0. Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+function getListStudents(id, firstName, location) {
+    const array = [];
+    array.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+    array.push({ id: 2, firstName: 'James', location: 'Columbia' });
+    array.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+    return array;
+}
+
+export default getListStudents
