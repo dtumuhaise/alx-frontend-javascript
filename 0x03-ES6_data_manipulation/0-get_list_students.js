d833bbed4a8c01c3be4fe6a9924866172186d94f
Davis Tumuhaise <tumuhaise@gmail.com>
2023-07-24 19:50:32 +0300

    fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 function getListStudents(id, firstName, location) {
-    const array = [];
-    array.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
-    array.push({ id: 2, firstName: 'James', location: 'Columbia' });
-    array.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
-    return array;
+  const array = [];
+  array.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  array.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  array.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+  return array;
 }
 
-export default getListStudents
+export default getListStudents;
